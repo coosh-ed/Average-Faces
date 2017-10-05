a10805ecd44fcb1d7ba686634c4012d395096ae9
colm <s1673102@sms.ed.ac.uk>
2017-10-05 21:56:50 +0100

    transform pictures

--- helpers/get_pics.py
+++ helpers/get_pics.py
@@ -1,10 +1,31 @@
-import urllib2
-import sys
+import urllib, urllib2, json
+import sys, os
 from bs4 import BeautifulSoup
 
 from female_TDs import list_female_TDs
 
-print list_female_TDs()
+
+def get_image_url(name):
+    header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
+    name = name.encode('utf-8').strip()
+    keyword = name + " TD"
+    query = "+".join(keyword.split())
+    url_googleimages = "https://www.google.ie/search?q="+query+\
+"&source=lnms&tbm=isch&sa=X&ved=0ahUKEwii1tyIgNrWAhWKJVAKHRHIDmMQ_AUICigB&biw=1280&bih=624" 
+    urlopen = urllib2.urlopen(urllib2.Request(url_googleimages,headers=header))
+    soup = BeautifulSoup(urlopen,'html.parser')
+    src = json.loads(soup.find("div",{"class":"rg_meta"}).text)['ou']
+    return src
+
+
+names = list_female_TDs()
+
+#create folders to save pictures
+if not os.path.exists('maleTDs'):
+    os.makedirs('maleTDs')
+if not os.path.exists('femaleTDs'):
+    os.makedirs('femaleTDs')
+
 
 URL = "https://www.kildarestreet.com/tds/"
 
@@ -12,6 +33,13 @@ response = urllib2.urlopen(URL)
 page_source = response.read()
 soup = BeautifulSoup(page_source)
 
-for img in soup.find_all('img'):
-    print img['src']
+tbl = soup.tbody
+
+for tr in soup.find_all('tr'):
+    name = tr.find('a').text
+    x = "female" if name in names else "male" 
+    
+    img_url = get_image_url(name)
+    urllib.urlretrieve(img_url, x+"TDs/"+os.path.basename(img_url))
+    
 
---
+++ transform_pictures.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   The face detector we use is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset (see
+#   https://ibug.doc.ic.ac.uk/resources/facial-point-annotations/):  
+#      C. Sagonas, E. Antonakos, G, Tzimiropoulos, S. Zafeiriou, M. Pantic. 
+#      300 faces In-the-wild challenge: Database and results. 
+#      Image and Vision Computing (IMAVIS), Special Issue on Facial Landmark Localisation "In-The-Wild". 2016.
+
+import sys
+import os
+import dlib
+import glob
+from skimage import io
+
+if len(sys.argv) != 3:
+    print(
+        "Give the path to the trained shape predictor model as the first "
+        "argument and then the directory containing the facial images.\n"
+        "For example, if you are in the python_examples folder then "
+        "execute this program by running:\n"
+        "    ./face_landmark_detection.py shape_predictor_68_face_landmarks.dat ../examples/faces\n"
+        "You can download a trained facial shape predictor from:\n"
+        "    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2")
+    exit()
+
+"""TO DO:
+* create folders for transformed pics
+* calculate transform matrix
+* transform the images and save them """
+
+predictor_path = sys.argv[1]
+faces_folder_path = sys.argv[2]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+#win = dlib.image_window()
+
+for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+
+    dets = detector(img, 1)
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        print "Part 36: {}, Part 45: {}".format(shape.part(36), shape.part(45))
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                  shape.part(1)))
+
+
+
